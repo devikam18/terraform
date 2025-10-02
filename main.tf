141bcb4364ef6dc4c8ad13f1ef29965c5d693b8c
devikam18 <devika18022002@gmail.com>
2025-10-02 14:52:09 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -167,14 +167,10 @@ resource "aws_iam_role_policy" "ec2_policy" {
   })
 }
 
-resource "aws_iam_instance_profile" "ec2_profile" {
-  name = "ec2_profile"
+resource "aws_iam_instance_profile" "ec2_profile_latest" {
+  name = "ec2_profile_latest"
   role = aws_iam_role.ec2_role.name
 
-  lifecycle {
-    prevent_destroy = true       # Terraform cannot delete it
-    ignore_changes  = [name]    # Terraform ignores name conflicts
-  }
 }
 
 ############################################
